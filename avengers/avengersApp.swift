f97026ee4c32aed7750139b1f7b233b4e3d4f4ba
Fabio Franzese <fabiofranzeseuni@gmail.com>
2024-10-09 11:17:06 +0200

    Initial Commit

---
+++ avengers/avengersApp.swift
@@ -0,0 +1,17 @@
+//
+//  avengersApp.swift
+//  avengers
+//
+//  Created by Fabio on 09/10/24.
+//
+
+import SwiftUI
+
+@main
+struct avengersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
